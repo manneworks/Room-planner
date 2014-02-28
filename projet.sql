1d302aa3c6201cbb6f489d537b26b1ab4db26d50
GuillaumeCo <copinguillaume@hotmail.fr>
2014-02-28 23:26:52 +0100

    modification table utilisateurs

--- projet.sql
+++ projet.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- GÃ©nÃ©rÃ© le: Ven 21 FÃ©vrier 2014 Ã  13:17
+-- GÃ©nÃ©rÃ© le: Ven 28 FÃ©vrier 2014 Ã  22:25
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.12
 
@@ -25,16 +25,30 @@ USE `projet`;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `enregistrements`
+--
+
+CREATE TABLE IF NOT EXISTS `enregistrements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` text NOT NULL,
+  `nom` text NOT NULL,
+  `data` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `objets`
 --
 
 CREATE TABLE IF NOT EXISTS `objets` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` text NOT NULL,
   `categorie` text NOT NULL,
   `lien` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `objets`
@@ -45,9 +59,30 @@ INSERT INTO `objets` (`id`, `nom`, `categorie`, `lien`) VALUES
 (2, 'Chaise', 'Cuisine', ''),
 (3, 'Lavabo', 'Salle de bain', ''),
 (4, 'Table', 'Cuisine', ''),
-(5, 'Lavabo', 'Cuisine', ''),
-(6, 'test', '', ''),
-(7, 'last', 'fin', '');
+(5, 'Lavabo', 'Cuisine', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` text NOT NULL,
+  `motDePasse` text NOT NULL,
+  `nom` text NOT NULL,
+  `prenom` text NOT NULL,
+  `fonction` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `pseudo`, `motDePasse`, `nom`, `prenom`, `fonction`) VALUES
+(24, 'Guiguicop', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'Copin', 'Guillaume', 'architecte');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
