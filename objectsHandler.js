fa37c1da10819fdd25ce354accc50b12683fe1a7
Irrek <lamothefrancois@sfr.fr>
2014-03-04 11:52:34 +0100

    Create objectsHandler.js

---
+++ objectsHandler.js
@@ -0,0 +1,69 @@
+
+
+function ObjectHandler(){
+	this.objs = [];
+}
+
+ObjectHandler.prototype.loadObjs = function(){
+	this.waitUILoaded(this);
+}
+
+
+ObjectHandler.prototype.getMesh = function(name){
+	var path = "models/"+name+".js";
+	for(var i=0;i<this.objs.length;i++){
+		console.log(this.objs[i].getName()+" "+path);
+		if(this.objs[i].getName()==path){
+			return this.objs[i].getMesh();
+		}
+	}
+	console.log("Error obj not found");
+	return null;
+}
+
+ObjectHandler.prototype.load = function(){
+	var objs = this.objs;
+	var objsElements = document.getElementsByClassName("models"); 
+	var i = objsElements.length; 
+	while(i--){
+		var path = "models/"+objsElements[i].id+".js";
+		var mat = new THREE.MeshLambertMaterial( { color: 'grey', side:THREE.DoubleSide, shading: THREE.SmoothShading } );
+
+	    loader.load( path, this.makeHandler(path,mat,objs));
+ 	}
+}
+
+ObjectHandler.prototype.makeHandler = function(path , mat, objs){
+	return function(geometry) {
+	        mesh = new THREE.Mesh( geometry, mat );
+	        geometry.computeFaceNormals();
+	        mesh.position.set(0, 0, 0);
+	       // var s = 10;
+	        //mesh.scale.set(s, s, s);
+	        objs.push(new myObj(path,mesh));
+    };
+}
+
+ObjectHandler.prototype.waitUILoaded = function(self){
+	console.log(uiLoaded);
+	if (!uiLoaded){
+	   setTimeout(function(){
+	   		self.waitUILoaded(self);
+	   },100);
+	} else {
+	  	self.load();
+	}
+}
+
+function myObj(name, mesh){
+	this.name=name;
+	this.mesh=mesh;
+
+	this.getName = function(){
+		return name;
+	}
+
+	this.getMesh = function(){
+		return mesh;
+	}
+}
